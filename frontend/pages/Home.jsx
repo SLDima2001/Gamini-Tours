9da6ecfd7e7bcb37b10667a10aa6131eb1d2f2e1
Dimalsha Praveen <dimalshapraveen2001@gmail.com>
2024-08-12 23:55:14 +0530

    Update Home.jsx

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -266,7 +266,7 @@ function Home() {
     padding: '20px ',
     borderRadius: '20px',
     boxShadow: '0 4px 8px rgba(0, 1, 1, 1)',
-    width: isMobile ? '20%' : 'auto', // Adjust width for mobile and PC views
+    width: isMobile ? '780px' : 'auto', // Adjust width for mobile and PC views
     height:'auto',
     borderColor:'',
   };
